435f6f9f3272c43dc1b0e41993c812630a737ee2
pravdomil <pravdomil.toman@gmail.com>
2016-03-06 00:13:05 +0100

    user modules system in userData

--- _renderer/core/Module/Module.js
+++ _renderer/core/Module/Module.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var fs = require('fs')
+var ncp = null // lazy load
 var path = require('path')
 
 var style = `
@@ -14,25 +15,29 @@ var style = `
 module.exports = class Module extends WebComponentAbstract {
 	initCallback() {
 		this.defaultConf()
-		this.loadModules()
 		
-		this.renderCss(style)
+		defineAppEvent('newModule', 'Adds new module to module list', 'Modules', "''")
+		self.on('newModule', e => (this.textarea.value += "\n" + e.d) && this.textarea.dispatchEvent(new Event('input')))
 		
-		this.el = this.newElement('class')
-		this.el.classList.add('column-system')
-		this.left = this.el.newElement('div')
-		this.right = this.el.newElement('div')
+		this.loadUserModulesDir().then(res => {
+			this.loadModules()
+			
+			this.renderCss(style)
+			
+			this.el = this.newElement('div')
+			this.el.classList.add('column-system')
+			this.left = this.el.newElement('div')
+			this.right = this.el.newElement('div')
+			
+			this.left.newElement('h1').textContent = 'Module list'
+			this.left.newElement('p').innerHTML = 'Edit module list as you want.<br>Be careful load only trusted modules!'
+			this.textarea = this.left.newElement('p').newElement('textarea', true, { value: this.ser.conf })
+			this.textarea.on('input', e => this.ser.conf = this.textarea.value)
+			this.left.newElement('p').innerHTML = `<a onclick="location.reload()" class="button">Reload to take effect</a>`
 		
-		this.left.newElement('h1').textContent = 'Module list'
-		this.left.newElement('p').innerHTML = 'Edit module list as you want.<br>Be careful load only trusted modules!'
-		this.textarea = this.left.newElement('p').newElement('textarea')
-		this.textarea.value = this.ser.conf
-		this.textarea.on('input', e => this.ser.conf = this.textarea.value)
-		this.left.newElement('p').innerHTML = `<p><a onclick="location.reload()" class="button">Reload to take effect</a></p>`
-		
-		var exampleFile = require.resolve('Example')
-		this.right.newElement('h1').textContent = 'Modules development'
-		this.right.newElement('div').innerHTML = `
+			var exampleFile = require.resolve('Example')
+			this.right.newElement('h1').textContent = 'Modules development'
+			this.right.newElement('div').innerHTML = `
 <ol>
 	<li><a onclick="require('electron').shell.showItemInFolder('${exampleFile}')" class="button">Take Example.js</a></li>
 	<li>Put some code in it</li>
@@ -42,10 +47,25 @@ module.exports = class Module extends WebComponentAbstract {
 	<li><a href="${App.package.pullRequest}" class="button">Make a pull request</a></li>
 	<li>Spread the word!</li>
 </ol>
-		`
+			`
+		})
+	}
+	
+	loadUserModulesDir() {
+		var userModulesDir = require('remote').app.getPath('userData') + '/node_modules'
+		module.paths.unshift(userModulesDir)
 		
-		defineAppEvent('newModule', 'Adds new module to module list', 'Modules', "''")
-		self.on('newModule', e => (this.textarea.value += "\n" + e.d) && this.textarea.dispatchEvent(new Event('input')))
+		return new Promise(resolve => {
+			fs.stat(userModulesDir, e => {
+				if(!e) return resolve()
+					
+				fs.mkdir(userModulesDir, e => {
+					if(e) throw e
+					ncp = ncp || require('ncp')
+					ncp(__dirname + "/Example", userModulesDir + '/Example', e => { if(e) throw e; resolve() })
+				})
+			})
+		})
 	}
 	
 	defaultConf() {
@@ -59,10 +79,7 @@ module.exports = class Module extends WebComponentAbstract {
 	}
 	
 	loadModules() {
-		this.ser.conf.split("\n").forEach(s => {
-			if(!s) return
-			try { App.newElement(require(s), false) } catch(e) { AppEvent('error', e) }
-		})
+		this.ser.conf.split("\n").forEach(s => s && setImmediate(() => App.newElement(require(s), false)) )
 	}
 	
 	readyCallback() {
