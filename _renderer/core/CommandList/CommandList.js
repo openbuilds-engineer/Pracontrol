f3b9f422993b5c0f592213cc9266e60f196aa26c
pravdomil <pravdomil.toman@gmail.com>
2016-03-12 19:56:12 +0100

    new CommandList

--- _renderer/core/CommandList/CommandList.js
+++ _renderer/core/CommandList/CommandList.js
@@ -1,40 +1,63 @@
 'use strict'
 
-var gCode = require('./gCode').commands
-
 var style = `
 :host {
-	display: block;
-	height: 100%;
-	overflow-x: hidden;
-	overflow-y: auto;
+	top: -1rem; bottom: -1rem; left: -1rem; right: -1rem;
+	position: absolute;
 }
 
 ::content {
 	
+	left {
+		flex: 0 0 auto;
+		margin-bottom: 0;
+		
+		height: 100%;
+		overflow-x: hidden;
+		overflow-y: auto;
+		
+		tab {
+			padding: 0.2rem 0.3rem;
+			color: #fff;
+			cursor: pointer;
+			font-size: 0.85em;
+			background-color: #333;
+			display: block;
+		
+			&.active, &:hover { background-color: #666; }
+		}
+	}
+	right {
+		height: 100%;
+		overflow-x: hidden;
+		overflow-y: auto;
+		top: 1rem;
+	}
 }
 `
 
-module.exports = class CommandList extends WebComponentAbstract {
+module.exports = class CommandList extends TabComponent {
 	initCallback() {
-		this.renderCss(style)
-		
-		// not implemented
-		// this.searchBox = this.newElement(require('./searchBox'), false)
+		this.renderLess(style)
 		
 		defineAppEvent('newCommand', 'Add new item to commands table', 'Commands', "{ name: '', desc: '', cat: '', arg: '' }")
 		self.on('newCommand', e => this.newCommand(e.d))
 		
-		this.table = this.newElement('table')
+		var div = this.newElement('div')
+		div.classList.add('column-system')
+		this.left = div.newElement('left')
+		this.right = div.newElement('right')
+		
+		this.tabInit('CommandTab', this.left)
 	}
 	
 	readyCallback() {
 		AppEvent('newTab', { instance: this, title: 'Commands', priority: -70 })
+		AppEvent('toggleTab', this)
 	}
 	
 	attachedCallback() {
-		this.categories = []
-		this.commands = []
+		var gCode = require('./gCode').commands
 		
 		Object.keys(gCode).forEach(code => {
 			var val = gCode[code]
@@ -43,23 +66,17 @@ module.exports = class CommandList extends WebComponentAbstract {
 	}
 	
 	newCommand(c) {
-		var tbody = this.querySelector(`tbody[cat='${c.cat}']`)
+		var cat = this.left.querySelector(`tab[name='${c.cat}']`)
+		var instance = null
 		
-		if(!tbody) {
-			var thead = this.table.newElement('thead')
-			
-			var tr = thead.newElement('tr')
-			var th = tr.newElement('th')
-			th.colSpan = 2
-			th.textContent = c.cat
-			
-			tbody = this.table.insertAfter(this.newElement('tbody', false), thead)
-			tbody.setAttribute('cat', c.cat)
+		if(!cat) {
+			instance = this.right.newElement('div')
+			AppEvent('newCommandTab', { instance: instance, title: c.cat })
+		}
+		else {
+			instance = cat.instance
 		}
 		
-		var tr = tbody.newElement('tr')
-		tr.newElement('td').textContent = c.name
-		tr.newElement('td').textContent = c.desc
-		tr.newElement('td').textContent = c.arg
+		instance.newElement('p').innerHTML = `<b>${c.name}</b> ${c.desc}`
 	}
 }
