29c096dcedaeab97ddda72668823b8ba5cf306b7
pravdomil <pravdomil.toman@gmail.com>
2016-03-12 17:28:17 +0100

    whitespace

--- _renderer/core/Module/Example/Example.js
+++ _renderer/core/Module/Example/Example.js
@@ -2,14 +2,14 @@
 
 var style = `
 :host {
-  
+	
 }
 
 ::content {
-  button {
-    display: block;
-    height: 100%;
-    width: 100%;
+	button {
+		display: block;
+		height: 100%;
+		width: 100%;
 		font-size: 2em;
 		color: #999;
 		background: repeating-linear-gradient(
@@ -20,32 +20,32 @@ var style = `
 			lighten(#000, 97%) 11px,
 			lighten(#000, 97%) 20px
 		);
-  }
+	}
 }
 `
 
 module.exports = class NameOfYourModule extends WebComponentAbstract {
-  initCallback() {
-    // work here
-    // do not dispatch events here
-    this.renderLess(style)
-    
-    this.but = this.newElement('button')
-    this.but.textContent = "Emergency stop"
-    this.but.on('click', e => AppEvent('serialWrite', { data: 'M999', prepend: true }))
-  }
-  
-  readyCallback() {
-    // app is ready
-    // fire events here
-    
-    // in this case we are going for new tab
-    AppEvent('newTab', { instance: this, 'title': 'Example' })
-  }
-  
-  attachedCallback() {
-    // additional work
-    // fired after element is attached to DOM
-    
-  }
+	initCallback() {
+		// work here
+		// do not dispatch events here
+		this.renderLess(style)
+		
+		this.but = this.newElement('button')
+		this.but.textContent = "Emergency stop"
+		this.but.on('click', e => AppEvent('serialWrite', { data: 'M999', prepend: true }))
+	}
+	
+	readyCallback() {
+		// app is ready
+		// fire events here
+		
+		// in this case we are going for new tab
+		AppEvent('newTab', { instance: this, 'title': 'Example' })
+	}
+	
+	attachedCallback() {
+		// additional work
+		// fired after element is attached to DOM
+		
+	}
 }
