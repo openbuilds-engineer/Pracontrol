a44a2a3651cbc35a97aab38de3cae487861da867
pravdomil <pravdomil.toman@gmail.com>
2016-03-13 14:09:22 +0100

    Restore saved coordinates button for All

--- _renderer/core/CommandList/UI/Position memory.js
+++ _renderer/core/CommandList/UI/Position memory.js
@@ -6,13 +6,16 @@ module.exports = class PositionMemory extends WebComponentAbstract {
 		var p = this.newElement('p')
 		p.newElement('a', true, { textContent: 'Save current coordinates', className: 'button' }).on('click', e => this.send('G60'))
 		
+		var p = this.newElement('p')
+		p.newText('Restore saved coordinates')
 		p.newElement('br')
-		p.newText('Restore saved coordinates ')
 		p.newElement('a', true, { textContent: 'X', className: 'button' }).on('click', e => this.send('G61 X'))
 		p.newText(' ')
 		p.newElement('a', true, { textContent: 'Y', className: 'button' }).on('click', e => this.send('G61 Y'))
 		p.newText(' ')
 		p.newElement('a', true, { textContent: 'Z', className: 'button' }).on('click', e => this.send('G61 Z'))
+		p.newText(' ')
+		p.newElement('a', true, { textContent: 'All', className: 'button' }).on('click', e => this.send('G61 XYZ'))
 	}
 	
 	send(val) {
