bf88138947584935d1c46b66cbffe80b216d1781
pravdomil <pravdomil.toman@gmail.com>
2016-03-14 12:22:14 +0100

    Setting UI fix M503 M501

--- _renderer/core/CommandList/UI/Settings.js
+++ _renderer/core/CommandList/UI/Settings.js
@@ -3,15 +3,15 @@
 module.exports = class Settings extends WebComponentAbstract {
 	initCallback() {
 		
-		var p = this.newElement('p')
-		p.newElement('a', true, { textContent: 'Read saved settings', className: 'button' }).on('click', e => this.send('M503'))
+		var p = this.newElement('p', true, { textContent: 'Revert to ' })
+		p.newElement('a', true, { textContent: 'Saved settings', className: 'button' }).on('click', e => this.send('M501'))
 		p.newText(' ')
 		p.newElement('a', true, { textContent: 'Factory default', className: 'button' }).on('click', e => this.send('M502'))
 		
 		var p = this.newElement('p', true, { textContent: "Current settings " })
 		p.newElement('a', true, { textContent: 'Save', className: 'button' }).on('click', e => this.send('M500'))
 		p.newText(' ')
-		p.newElement('a', true, { textContent: 'Read', className: 'button' }).on('click', e => this.send('M501'))
+		p.newElement('a', true, { textContent: 'Read', className: 'button' }).on('click', e => this.send('M503'))
 	}
 	
 	send(val) {
