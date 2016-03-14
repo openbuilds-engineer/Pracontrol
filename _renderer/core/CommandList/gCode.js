dce1207344f2826f6ef764b4a9b74868bc9bc253
pravdomil <pravdomil.toman@gmail.com>
2016-03-14 10:07:29 +0100

    reorder

--- _renderer/core/CommandList/gCode.js
+++ _renderer/core/CommandList/gCode.js
@@ -23,8 +23,8 @@ module.exports = [
 	{ tab: "Control", cat: "Laser", gcode: "M5", name: "Turn off laser" },
 	
 	{ tab: "Settings", cat: "Speed", gcode: "M92", name: "Axis steps per unit", arg: "X<#> Y<#> Z<#> E<#>" },
-	{ tab: "Settings", cat: "Speed", gcode: "M201", name: "Max acceleration for print moves", arg: "X<#> Y<#> Z<#> E<#> (s^2)" },
 	{ tab: "Settings", cat: "Speed", gcode: "M203", name: "Maximum feedrate", arg: "X<#> Y<#> Z<#> E<#> (mm/sec)" },
+	{ tab: "Settings", cat: "Speed", gcode: "M201", name: "Max acceleration for print moves", arg: "X<#> Y<#> Z<#> E<#> (s^2)" },
 	{ tab: "Settings", cat: "Speed", gcode: "M204", name: "Acceleration set", arg: "P<#> R<#> T<#> (Printing Retract Travel) (mm/sec^2)" },
 	{ tab: "Settings", cat: "Speed", gcode: "M205", name: "Advanced settings set", arg: "S = Min feed rate (mm/s) T = Min travel feed rate (mm/s) B = Min segment time (µs) X = Max XY jerk (mm/s/s) Z = Max Z jerk (mm/s/s) E = Max E jerk (mm/s/s)" },
 	{ tab: "Settings", cat: "Speed", gcode: "M220", name: "Speed factor set", arg: "S<0-100>" },
