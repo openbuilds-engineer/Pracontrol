73e55ee23b98d3da73aa30e82abff76324a7d512
pravdomil <pravdomil.toman@gmail.com>
2016-03-14 10:11:17 +0100

    Move UI update

--- _renderer/core/CommandList/UI/Move.js
+++ _renderer/core/CommandList/UI/Move.js
@@ -2,7 +2,7 @@
 
 var style = `
 ::content label { white-space: nowrap; }
-::content input { width: 40%; }
+::content input[type='number'] { width: 5rem; }
 `
 
 module.exports = class Move extends WebComponentAbstract {
@@ -12,22 +12,18 @@ module.exports = class Move extends WebComponentAbstract {
 		var p = this.newElement('p')
 		
 		var opt = { type: 'number', step: 1, value: 0 }
-		
 		var X = p.newElement('label', true, { textContent: 'X '}).newElement('input', true, opt)
 		X.on('input', e => this.move({ X: X.value }))
-		
 		p.newText(' ')
 		
 		var Y = p.newElement('label', true, { textContent: 'Y '}).newElement('input', true, opt)
 		Y.on('input', e => this.move({ Y: Y.value }))
-		
 		p.newElement('br')
 		
 		opt.step *= .1
 		
 		var Z = p.newElement('label', true, { textContent: 'Z '}).newElement('input', true, opt)
 		Z.on('input', e => this.move({ Z: Z.value }))
-		
 		p.newText(' ')
 		
 		var E = p.newElement('label', true, { textContent: 'E '}).newElement('input', true, opt)
