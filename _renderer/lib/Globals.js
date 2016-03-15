b6bdd47752fe5d61545d6533ed00370ddf945486
pravdomil <pravdomil.toman@gmail.com>
2016-03-15 16:54:14 +0100

    not needed

--- _renderer/lib/Globals.js
+++ _renderer/lib/Globals.js
@@ -1,19 +1,7 @@
 'use strict'
 
-exports.defineAppEvent = function(name, desc, cat, arg) {
-	
-	var func = () => { AppEvent('defineAppEvent', { name, desc, arg, cat } ) }
-	
-	('App' in self && App.ready) ? func() : self.on('appReady', e => func())
-}
-
-exports.defineKeyShortcut = function(key, desc, cat) {
-	
-	var func = () => { AppEvent('defineKeyShortcut', { key, desc, cat } ) }
-	
-	('App' in self && App.ready) ? func() : self.on('appReady', e => func())
-}
-
+exports.defineAppEvent = (name, desc, cat, arg) => AppEvent('defineAppEvent', { name, desc, arg, cat } )
+exports.defineKeyShortcut = (key, desc, cat) => AppEvent('defineKeyShortcut', { key, desc, cat } )
 exports.AppEvent = function(name, detail) {
 	var e = new Event(name)
 	
