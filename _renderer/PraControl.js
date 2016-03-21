e3858723b3a4f27c4476a9cb5b0edb811b978a90
pravdomil <pravdomil.toman@gmail.com>
2016-03-21 15:13:36 +0100

    whitespace

--- _renderer/PraControl.js
+++ _renderer/PraControl.js
@@ -1,20 +1,20 @@
 'use strict'
 
 class PraControl extends require('web-component-app') {
-	initCallback() {
-		// package info
-		this.package = require('../package.json')
-		
-		// core modules
-		this.core = ['About', 'CommandList', 'Console', 'Dashboard', 'Developer', 'KeyShortcutsTable', 'Module', 'Panel', 'Serial', 'Printer', 'Tab']
-		
-		// error reporting here, must load first
-		this.newElement(require('UI/UI'), false)
-		
-		// render css then load content
-		this.lessOpt = { paths: [require('rembased'), __dirname + '/res'] }
-		this.renderLess(__dirname + '/res/style.less', { global: true }).then(e => this.appMain())
-	}
+  initCallback() {
+    // package info
+    this.package = require('../package.json')
+    
+    // core modules
+    this.core = ['About', 'CommandList', 'Console', 'Dashboard', 'Developer', 'KeyShortcutsTable', 'Module', 'Panel', 'Serial', 'Printer', 'Tab']
+    
+    // error reporting here, must load first
+    this.newElement(require('UI/UI'), false)
+    
+    // render css then load content
+    this.lessOpt = { paths: [require('rembased'), __dirname + '/res'] }
+    this.renderLess(__dirname + '/res/style.less', { global: true }).then(e => this.appMain())
+  }
 }
 
 global.App = document.body.newElement(PraControl)
--- distribution/prebuild.js
+++ distribution/prebuild.js
@@ -15,28 +15,28 @@ var child_process = require('child_process')
 process.chdir(path.join(__dirname, '..'))
 
 if(process.platform == "win32") {
-	
-	// update packages
-	child_process.execSync('npm update -g electron-prebuilt', { stdio: [0, 1, 2] })
-	child_process.execSync('npm update -g electron-rebuild', { stdio: [0, 1, 2] })
-	
-	// rebuild
-	child_process.execSync('electron-rebuild -w serialport -m . -f', { stdio: [0, 1, 2] })
-	
-	// correct naming
-	var bin = path.join(path.dirname(require.resolve('serialport')), 'build', 'Release')
-	var getDirectories = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory())
-	
-	getDirectories(bin).forEach(p => {
-		if(!p.match(/^electron-/)) return
-		var o = path.join(bin, p)
-		var n = path.join(bin, p.replace(/electron-[^-]+/, `node-v${process.versions.modules}`))
-		fs.renameSync(o, n)
-	})
-	
+  
+  // update packages
+  child_process.execSync('npm update -g electron-prebuilt', { stdio: [0, 1, 2] })
+  child_process.execSync('npm update -g electron-rebuild', { stdio: [0, 1, 2] })
+  
+  // rebuild
+  child_process.execSync('electron-rebuild -w serialport -m . -f', { stdio: [0, 1, 2] })
+  
+  // correct naming
+  var bin = path.join(path.dirname(require.resolve('serialport')), 'build', 'Release')
+  var getDirectories = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory())
+  
+  getDirectories(bin).forEach(p => {
+    if(!p.match(/^electron-/)) return
+    var o = path.join(bin, p)
+    var n = path.join(bin, p.replace(/electron-[^-]+/, `node-v${process.versions.modules}`))
+    fs.renameSync(o, n)
+  })
+  
 } else if(process.platform == "darwin") {
-	
-	// rebuild
-	process.chdir(path.dirname(require.resolve('serialport')))
-	child_process.execSync('npm run install')
+  
+  // rebuild
+  process.chdir(path.dirname(require.resolve('serialport')))
+  child_process.execSync('npm run install')
 }
