cd868674dcef4daa4008a40ec61cadeca1b3b7a4
pravdomil <pravdomil.toman@gmail.com>
2016-04-01 13:23:00 +0200

    new build system

--- distribution/build.js
+++ distribution/build.js
@@ -1,9 +1,34 @@
 'use strict'
 
-var path = require('path')
+process.chdir(__dirname + '/..')
+var json = require(__dirname + '/../package.json')
 
-process.chdir(path.join(__dirname, '..'))
+var opt = {
+  'dir': '.', 'out': 'distribution',
+  'overwrite': true,
+  'ignore': [
+    '^(?!$|/_main|/_renderer|/node_modules|/package.json$)',
+    '/\\.[^/]*'
+  ],
+  
+  'arch': 'x64', 'platform': 'win32,darwin',
+  'asar': true, 'prune': true,
+  
+  'icon': 'resources/icon',
+  
+  'app-bundle-id': json.companyName.replace(/\..+/, '') + '.' + json.name,
+  'app-version': json.version,
+  'app-category-type': 'public.app-category.utilities',
+  'app-copyright': '',
+  
+  'version-string': {
+    'CompanyName': json.companyName,
+    'ProductName': json.productName,
+    'FileDescription': json.description.replace(/ /g, ' '),
+    'InternalName': '',
+    'OriginalFilename': '',
+    'LegalCopyright': '',
+  },
+}
 
-var pjson = require(path.join(__dirname, '../package.json'))
-
-require('electron-packager')(pjson.electronPackagerOpts, e => console.log(e ? e : 'done'))
+require('electron-packager')(opt, e => console.log(e ? e : 'done'))
