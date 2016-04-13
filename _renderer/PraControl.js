b984c86c5ca5a84b5d8ea5cd26f629d7fbf8b00f
pravdomil <pravdomil.toman@gmail.com>
2016-04-13 20:29:56 +0200

    core modules

--- _renderer/PraControl.js
+++ _renderer/PraControl.js
@@ -5,20 +5,20 @@ class PraControl extends require('web-component-app') {
     // package info
     this.package = require('../package.json')
     
-    // core modules
-    this.core = [
-      'Panel', 'Tab', 'Serial', 'Printer',
-      'About', 'Control', 'Console',
-      'Developer', 'KeyShortcutsTable', 'Module',
-      'ControlShortcuts', 'RemoteControl', '25D Slicer',
-    ]
-    
     // error reporting here, must load first
     this.newElem(require('UI/UI'), false)
     
     // render css then load content
     this.lessOpt = { paths: [require('rembased'), __dirname + '/res'] }
-    this.renderLess(__dirname + '/res/style.less', { global: true }).then(e => this.appMain())
+    this.renderLess(__dirname + '/res/style.less', { global: true }).then(e => this.appReady())
+  }
+  
+  readyCallback() {
+    // core modules
+    var elem = [
+      
+    ]
+    elem.forEach(e => setImmediate(() => this.newElem(require(e + '/' + e), false)))
   }
 }
 
