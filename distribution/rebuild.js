7c6fa013db74e6ab4a35f0a12f0fa1bd81f9494a
pravdomil <pravdomil.toman@gmail.com>
2016-08-29 17:20:40 +0200

    remove previous build

--- distribution/rebuild.js
+++ distribution/rebuild.js
@@ -4,6 +4,10 @@ var filepath = require('path')
 var execSync = require('child_process').execSync
 var fs = require('fs-promise')
 
+// remove previous build
+var serialportRoot = filepath.dirname(require.resolve('serialport')) + '/..'
+fs.removeSync(serialportRoot + '/build')
+
 // rebuild serialport
 var cmd = ''
 if(process.platform == 'darwin') cmd = 'node_modules/.bin/'
