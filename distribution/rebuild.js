2ea7d38fd3a72a301570f2766012f72987e1a7cc
pravdomil <pravdomil.toman@gmail.com>
2016-08-29 17:16:53 +0200

    new rebuild system

--- distribution/rebuild.js
+++ distribution/rebuild.js
@@ -1,22 +1,12 @@
-var path = require('path')
-var child_process = require('child_process')
-var serialportRoot = path.dirname(require.resolve('serialport')) + '/..'
 process.chdir(__dirname + '/..')
 
-// change work dir
-process.chdir(path.join(__dirname, '..'))
+var filepath = require('path')
+var execSync = require('child_process').execSync
+var fs = require('fs-promise')
 
-if(process.platform == 'win32') {
-  // delete current serialport build
-  child_process.execSync(`rmdir "${path.join(serialportRoot, '/build')}" /s /q`, { stdio: 'inherit' })
-  
-  // rebuild serialport
-  child_process.execSync('electron-rebuild -w serialport -m . -f', { stdio: 'inherit' })
-}
-else if(process.platform == 'darwin') {
-  // delete current serialport build
-  child_process.execSync(`rm -r "${path.join(serialportRoot, '/build')}"`, { stdio: 'inherit' })
-  
-  // rebuild serialport
-  child_process.execSync('node_modules/.bin/electron-rebuild -w serialport -m . -f', { stdio: 'inherit' })
-}
+// rebuild serialport
+var cmd = ''
+if(process.platform == 'darwin') cmd = 'node_modules/.bin/'
+cmd += 'electron-rebuild -w serialport -m . -f -v ' + execSync('npm view electron-prebuilt version')
+
+execSync(cmd, { stdio: 'inherit' })
